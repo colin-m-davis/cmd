b88fe2f33a84ff22dd29d2f93139bc2589fd0b1e
colindavis <colinmichaelsdavis@gmail.com>
2023-12-15 16:36:43 -0800

    initial print fns

---
+++ print.hpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <utility>
+
+namespace cmd {
+    void printd_arg(const auto& delim, bool is_first, const auto& arg) {
+        if (!is_first) std::cout << delim;
+        std::cout << arg;
+    }
+
+    template <std::size_t... I>
+    void printd_helper(const auto& delim, auto&& tup, std::index_sequence<I...>) {
+        (printd_arg(delim, I == 0, std::get<I>(tup)), ...);
+    }
+
+    void printd(const auto& delim, auto&&... args) {
+        printd_helper(
+            delim,
+            std::forward_as_tuple(args...),
+            std::make_index_sequence<sizeof...(args)>());
+        std::cout << '\n';
+    }
+
+    void print(auto&&... args) {
+        printd(' ', args...);
+    }
+
+    
+};
